94d9867930d84d2bf46115301ee9a1494537ea30
Roman <krasevych@gmail.com>
2017-04-24 14:46:35 +0300

    add

--- test/spec/test.js
+++ test/spec/test.js
@@ -6,10 +6,10 @@
    describe('Give it some context', function () {
       describe('maybe a bit more context here: ', function () {
          it('should ', function () {
-            expect(4).toBe(3)
+            expect(4).toBe(4)
          });
          it('should run here few assertions', function () {
-            expect(4).toBe(4)
+            expect(4).toBe(3)
          });
       });
    });
